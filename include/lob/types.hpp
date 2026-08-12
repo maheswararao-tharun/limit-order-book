111fd0f8b0915521e3fe7a5cacd404906604b499
tharun-maheswararao <tharun.m1807@gmail.com>
2026-08-11 20:40:31 -0400

    Add core types and update gitignore for clangd cache

---
+++ include/lob/types.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <cstdint>
+
+namespace lob {
+
+    // Declared all the types for Order Book Engine
+    // Book -> struct
+    // Limit -> BuyTree & SellTree -> Map(Red-Black Tree)
+    // Side -> Buy or Sell -> enum class
+    // Order -> Linked List
+
+    using OrderId = std::uint64_t;
+
+    enum class Side : std::uint8_t {
+        buy,
+        sell
+    };
+
+    using Price = std::int64_t;
+
+    using Timestamp = std::uint64_t;
+
+    using Quantity = std::uint32_t;
+
+    struct Order {
+        OrderId id;
+        Side side;
+        Price price;
+        Quantity quantity;
+        Timestamp entryTime;
+    };
+
+}
+
+
+
+
+
