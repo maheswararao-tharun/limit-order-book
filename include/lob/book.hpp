57d7a0262b6ee3f5be953157ce5431d515b5a695
tharun-maheswararao <tharun.m1807@gmail.com>
2026-08-13 05:15:31 -0400

    WIP: matchOrder skeleton with safe empty-tree checks on both sides

--- include/lob/book.hpp
+++ include/lob/book.hpp
@@ -2,6 +2,7 @@
 #include "types.hpp"
 #include<optional>
 #include<map>
+#include<algorithm>
 
 namespace lob {
 
@@ -18,4 +19,52 @@ namespace lob {
         bool updateHighestBuy();
         bool updateLowestSell();
     };
+
+    void Book::addOrder(Order order) {
+        // your code here
+        if(order.side == Side::buy) {
+            buyTree[order.price].orders.push_back(order);
+        } else {
+            sellTree[order.price].orders.push_back(order);
+        }
+    }
+
+    std::optional<Order> Book::matchOrder(Order incomingOrder) {
+        // your code here
+        
+        Quantity tradeQty, restingOrderSize, incomingOrderSize = incomingOrder.quantity;
+
+        if(incomingOrder.side == Side::buy) {
+            if(Book::sellTree.empty()) {
+                return incomingOrder;
+            } else {
+                Price lowestSellPrice = Book::lowestSell->first;
+                Order lowestSellOrder = Book::lowestSell->second.orders.front();
+                tradeQty = std::min(incomingOrder.quantity, lowestSellOrder.quantity);
+                restingOrderSize = lowestSellOrder.quantity - tradeQty;
+                incomingOrderSize -= tradeQty;
+                if(incomingOrderSize > 0) {
+
+                } if(incomingOrderSize == 0) {
+
+                }
+            }
+            
+        } else {
+            if(Book::buyTree.empty()) {
+                return incomingOrder;
+            } else {
+                Price highestBuyPrice = Book::highestBuy->first;
+                Order highestBuyOrder = Book::highestBuy->second.orders.front();
+                tradeQty = std::min(incomingOrder.quantity, highestBuyOrder.quantity);
+                restingOrderSize = highestBuyOrder.quantity - tradeQty;
+                incomingOrderSize -= tradeQty;
+                if(incomingOrderSize > 0) {
+
+                } if(incomingOrderSize == 0) {
+
+                }
+            }
+        }
+    }
 }
